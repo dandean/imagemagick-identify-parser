d5b8f6e209e556afe9ce3bffe310abc80b84b3fd
dandean <danwdean@gmail.com>
2011-11-30 23:04:59 -0800

    Updated unit tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,33 @@
-var Reader = require('../main');
+var reader = require('../main');
 var input = require('fs').readFileSync(require('path').join(__dirname, 'sample.txt'), 'utf8');
+var assert = require('assert');
+
+var result = reader(input);
 
 describe('Module', function(){
+  
   it('should convert input into an object', function(){
-    var result = new Reader(input);
-    console.log(result.data.Properties);
+    assert.ok(result instanceof Object);
+    assert.ok(result instanceof String === false);
+  });
+  
+  it('should retain a reference to the raw input', function() {
+    assert.ok(Object.prototype.toString.call(result.rawInput) === '[object String]');
+  });
+
+  it('should convert integer-like values to a Number', function() {
+    // Zero
+    assert.ok(Object.prototype.toString.call(result['Channel statistics'].Alpha.skewness) === '[object Number]');
+
+    // Negative
+    assert.ok(Object.prototype.toString.call(result['Channel statistics'].Alpha.kurtosis) === '[object Number]');
+  });
+
+  it('should convert float-like values to a Number', function() {
+    // Zero
+    assert.ok(Object.prototype.toString.call(result['Channel statistics'].Blue.skewness) === '[object Number]');
+
+    // Negative
+    assert.ok(Object.prototype.toString.call(result['Channel statistics'].Blue.kurtosis) === '[object Number]');
   });
 });
