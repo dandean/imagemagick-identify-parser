f2007c4736ca8bf5378a7d0a73291ac2ebfd05c3
Florian Sesser <fs@it-agenten.com>
2013-02-25 10:51:07 +0100

    Add tests for GraphicsMagick output data
    We moved from imagemagick to graphicsmagick since it has other/(for us) less
    daunting bugs; Alas, GM seems to vanilla copy JPEG meta data to it's output,
    confusing the best-meaning parsers.  Still, we want to get at least some key
    data (like geometry and format).
    
    Better yet, of course, would be to correctly parse/reconstruct as much data as
    possible, even after encountering some random, garbled input. This commit
    leaves that exercise for later.

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
-var reader = require('../main');
-var input = require('fs').readFileSync(require('path').join(__dirname, 'fixtures/sample.txt'), 'utf8');
+var fs = require('fs');
 var assert = require('assert');
 
+var reader = require('../main');
+
+var input = fs.readFileSync(require('path').join(__dirname, 'fixtures/sample.txt'), 'utf8');
+
 var result = reader(input);
+
 
 describe('Module', function(){
 
@@ -40,5 +44,55 @@ describe('Module', function(){
   it('should extract `width` and `height` properties from `geometry` property', function() {
     assert.ok(Object.keys(result).indexOf('width') > -1);
     assert.ok(Object.keys(result).indexOf('height') > -1);
+  });
+
+  it('should return sensible info for GraphicsMagick 1.3.16 input data', function() {
+    var identify_data_ok = fs.readFileSync(require('path').join(__dirname, 'fixtures/GraphicsMagick_1.3.16_identify_ok_1.txt'), 'utf8');
+    var id = reader(identify_data_ok);
+    // console.log(id);
+
+    assert(id);
+    assert.notDeepEqual(id, {}, "Output must not be empty");
+
+    assert.strictEqual(id.Geometry, '187x251');
+    assert.strictEqual(id.Format, 'JPEG (Joint Photographic Experts Group JFIF format)');
+    assert.strictEqual(id.Depth, '8 bits-per-pixel component');
+
+    assert.strictEqual(id.Type, 'true color');
+    assert.strictEqual(id['Channel Statistics'].Red.Minimum, '3.00 (0.0118)');
+  });
+
+  it('should return sensible info for slightly bogon GraphicsMagick_1 1.3.16 input data 1', function() {
+    var identify_data_bogon1 = fs.readFileSync(__dirname + "/fixtures/GraphicsMagick_1.3.16_identify_bogon_1.txt").toString();
+    var id = reader(identify_data_bogon1);
+    // console.log(id);
+
+    assert(id);
+    assert.notDeepEqual(id, {}, "Output must not be empty");
+
+    assert.strictEqual(id.Geometry, '536x468');
+    assert.strictEqual(id.Format, 'JPEG (Joint Photographic Experts Group JFIF format)');
+    assert.strictEqual(id.Depth, '8 bits-per-pixel component');
+
+    assert.strictEqual(id.Type, 'true color');
+    assert.strictEqual(id['Channel Statistics'].Red.Mean, '30.93 (0.1213)');
+  });
+
+  it('should return sensible info for slightly bogon GraphicsMagick_1 1.3.16 input data 2', function() {
+    var identify_data_bogon2 = fs.readFileSync(__dirname + "/fixtures/GraphicsMagick_1.3.16_identify_bogon_2.txt").toString();
+    var id = reader(identify_data_bogon2);
+    // console.log(id);
+
+    assert(id);
+    assert.notDeepEqual(id, {}, "Output must not be empty");
+
+    assert.strictEqual(id.Geometry, '601x800');
+    assert.strictEqual(id.Format, 'JPEG (Joint Photographic Experts Group JFIF format)');
+    assert.strictEqual(id.Depth, '8 bits-per-pixel component');
+
+    assert.strictEqual(id.Type, 'true color');
+
+    // TODO: Does not work yet
+    // assert.strictEqual(id['Profile-EXIF']['Date Time Digitized'], '2010:12:03 14:12:50');
   });
 });
